b522985c8ee42b51b3b5d6238885a065c102d50d
Sparsh Kumar <sparshk@bu.edu>
2017-02-07 18:44:58 -0500

    Added download.py
    Type a file into the terminal like "python download.py <filename-to-download>
    Will need to add error handling...

---
+++ homepi/weather/download.py
@@ -0,0 +1,21 @@
+import boto3
+import sys
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+#TODO: Make error handling
+
+file_to_download = sys.argv[1]
+
+# print 'Number of arguments:', len(sys.argv), 'arguments.'
+# print 'Argument List:', str(sys.argv)
+# print sys.argv[1]
+
+
+# Assumes bucket already exists
+s3.Bucket('buteamfourteen').download_file(file_to_download, 'america2.txt')
