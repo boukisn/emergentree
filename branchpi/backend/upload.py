14359c580aad17b6d3eff03347980e8d16cf2992
Sparsh Kumar <sparshk@bu.edu>
2017-02-07 18:49:23 -0500

    Add files via upload

---
+++ branchpi/backend/upload.py
@@ -0,0 +1,22 @@
+import boto3
+import sys
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+#TODO: Make error handling
+
+file_to_upload = sys.argv[1]
+
+# print 'Number of arguments:', len(sys.argv), 'arguments.'
+# print 'Argument List:', str(sys.argv)
+# print sys.argv[1]
+
+
+# Assumes bucket already exists
+data = open(file_to_upload, 'rb')
+s3.Bucket('buteamfourteen').put_object(Key=file_to_upload, Body=data)
