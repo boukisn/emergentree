fb5ec919b9caac85640081b29025994513f364b4
Sparsh Kumar <sparshk@bu.edu>
2017-03-18 18:01:35 -0400

    Update sms.py

--- homepi/sms.py
+++ homepi/sms.py
@@ -1,5 +1,15 @@
 import boto3
+import sys
 
 sns = boto3.client('sns')
-number = '+15856430318'
-sns.publish(PhoneNumber = number, Message='Tree Update: It is about to fall lol' )
+
+with open(sys.argv[1], "r") as ins:
+    array = []
+    for line in ins:
+        array.append(line)
+
+
+number = array[0]
+message = array[1]
+
+sns.publish(PhoneNumber = array[0], Message = array[1])
