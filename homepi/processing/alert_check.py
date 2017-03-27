bff5f19222ae9fc397ddafaeef692b445ea8c029
Soaresb <soaresb@bu.edu>
2017-03-26 22:15:35 -0400

    added alert check
    added alert check for angles,weather,timer

---
+++ homepi/processing/alert_check.py
@@ -0,0 +1,44 @@
+import sys
+import os.path
+import datetime
+
+filename = "/home/pi/emergentree/homepi/frontend/server/timer.log"
+writefile = open("/home/pi/emergentree/homepi/frontend/server/alerts.config","w+")
+if os.path.exists(filename):
+	f = open(filename,"r")
+	for line in f:
+		now = line
+	now2 = datetime.datetime.strptime(now, "%Y-%m-%d %H:%M:%S.%f")
+	delta = (datetime.datetime.now() - now2)
+	if delta.seconds // 3600 + delta.days*24 > 90:
+		writefile.write("WARNING,Battery,Your Battery is going to die soon\n")
+	f.close()
+
+else:
+	f = open(filename,"w+")
+	now = datetime.datetime.now()
+	f.write(str(now))
+	f.close()
+	
+alertfile ="/home/pi/emergentree/homepi/weather/advisories.log"
+alertfile_read = open(alertfile,"r")
+if os.stat(alertfile).st_size == 0:
+	pass
+else:
+	warning=""
+	for line in alertfile_read:
+		warning = line
+	my_list = warning.split(",")
+	writefile.write("WARNING,Weather Alert,"+my_list[1]+"\n")
+
+anglefile = open("/home/pi/emergentree/homepi/frontend/server/angle.config","r")
+anglealert=""
+for line in anglefile:
+	anglealert = line
+anglealert = anglealert.split(",")
+if float(anglealert[2]) >= 20:
+	writefile.write("WARNING,Tree Angle,Angle variance is >= 20\n")	
+
+writefile.close()
+anglefile.close()
+alertfile_read.close()
